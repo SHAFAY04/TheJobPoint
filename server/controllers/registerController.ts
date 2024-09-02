10389ed1600ae874413030b850005f6d6c8f13c9
Shafay <muhammadshafay2014@gmail.com>
2024-09-02 19:54:11 +0500

    made an express rest api using the mvc pattern (not related to the react project)

---
+++ server/controllers/registerController.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import users from '../model/users.json'
+import * as bcrypt from 'bcrypt'
+
+const userDB={
+
+    users:users,
+    setUsers:function(data){
+
+        this.users=data
+    }
+}
+
+const handleNewUser = async(req,res)=>{
+
+    const {user,pwd}=req.body
+
+    if(!user||!pwd){
+
+        res.status(400).json({message:'Username and Password are required!'})
+    }
+}
