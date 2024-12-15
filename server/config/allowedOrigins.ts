ad24bb5f64f77aa5bbd7c3187c0cb015fc123f65
Shafay <muhammadshafay2014@gmail.com>
2024-12-15 22:57:17 +0500

    added the testing url in allowed origins

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -2,6 +2,7 @@ export {}
 const whitelist=[
 
     'https://thejobpoint.netlify.app',
+    'https://oauth-testing--thejobpoint.netlify.app',
     'http://localhost:3000',
     'https://www.google.com',
     'http://127.0.0.1:5500'
